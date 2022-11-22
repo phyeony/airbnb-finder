3448c69b9828de9afccc36224eece1e719621169
jpa118 <jpa118@sfu.ca>
2022-11-21 22:24:58 -0800

    cleaned_airbnb

--- airbnb/airbnb_cleaning.py
+++ airbnb/airbnb_cleaning.py
@@ -5,5 +5,6 @@ airbnb = pd.read_csv('airbnb/airbnb_listings.csv.gz')
 # Data cleaning - only using the data we need
 airbnb = airbnb[['name', 'listing_url','neighbourhood', 'latitude', 'longitude', 'room_type', 'review_scores_rating']] 
 
+# Export to a cleaned csv file
 airbnb.to_csv("airbnb/filtered_airbnb_listings.csv", index=False)
 
