0aed7892aa4adff7f545941e114c9145713108e5
Pentaminum <scott0929@gmail.com>
2022-11-23 13:49:14 -0800

    filtered

--- airbnb/airbnb_filtering.py
+++ airbnb/airbnb_filtering.py
@@ -4,7 +4,7 @@ import sys
 # python airbnb_filtering.py 100 200 private
 def main(min_price, max_price, room_type):
     airbnb = pd.read_csv('cleaned_airbnb_listings.csv')
-    
+
     if room_type == "entire":
         room_type = "Entire home/apt"
     elif room_type == "hotel":
@@ -21,8 +21,9 @@ def main(min_price, max_price, room_type):
     # filter price range
     price_filtered = airbnb[(airbnb['price'] >= float(min_price)) & (airbnb['price'] <= float(max_price))]
     # filter room type
-    room_filtered = price_filtered[(price_filtered['room_type'] == room_type)]
-    print(room_filtered[['name', 'price', 'room_type']])
+    filtered = price_filtered[(price_filtered['room_type'] == room_type)]
+
+    filtered.to_csv(min_price + '_to_' + max_price + '_' + room_type + "_filtered_aribnb.csv" , index=False)
 
 if __name__ == '__main__':
     min_price = sys.argv[1]
