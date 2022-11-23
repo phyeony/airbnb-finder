c916f004a0fbf035b4d1a156d0146ba37e1d9d60
Pentaminum <scott0929@gmail.com>
2022-11-22 18:35:38 -0800

    airbnb

--- airbnb/airbnb_cleaning.py
+++ airbnb/airbnb_cleaning.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-airbnb = pd.read_csv('airbnb/airbnb_listings.csv.gz')
+airbnb = pd.read_csv('airbnb_listings.csv.gz')
 
 # Data cleaning - only using the data we need
 airbnb = airbnb[['name', 'listing_url','neighbourhood', 'latitude', 'longitude', 'room_type', 'review_scores_rating']] 
 
 # Export to a cleaned csv file
-airbnb.to_csv("airbnb/filtered_airbnb_listings.csv", index=False)
+airbnb.to_csv("filtered_airbnb_listings.csv", index=False)
 
--- airbnb/airbnb_filtering.py
+++ airbnb/airbnb_filtering.py
@@ -1,14 +1,18 @@
 import pandas as pd
+import sys
 
-airbnb = pd.read_csv('airbnb/filtered_airbnb_listings.csv')
+airbnb = pd.read_csv('filtered_airbnb_listings.csv')
 
-def main(min_price, max_price):
+def main(min_price, max_price, room_type):
     # filter price range
-    
+    print("min price: ", min_price)
+    print("max price: ", max_price)
+    print("room type: ", room_type)
+    print(airbnb)
     # filter room type
 
 if __name__ == '__main__':
     min_price = sys.argv[1]
     max_price = sys.argv[2]
     room_type = sys.argv[3]
-    main(min_price, max_price)
+    main(min_price, max_price, room_type)
