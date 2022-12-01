49c95d21eb679327643c12fdcc4fe888222b3aa4
phyeony <hyeonyoung_park@sfu.ca>
2022-12-01 00:22:46 -0800

    ongoing- add more osm data

--- data-analysis/osm/clean-up-leisure.py
+++ data-analysis/osm/clean-up-leisure.py
@@ -2,49 +2,19 @@
 import pandas as pd
 import numpy as np
 
-amenities = pd.read_json(
-    "output/amenity/amenities-vancouver.json.gz", lines=True, compression="gzip"
+df = pd.read_json(
+    "output/leisure/leisure.json.gz", lines=True, compression="gzip"
 )
 #%%
 
-#amenities['amenity'].unique()
+# df['type'].unique()
 
-# list = ['post_box', 'telephone', 'bench', 'toilets', 'bank', 'cafe',
-#    'fast_food', 'bbq', 'vending_machine', 'restaurant',
-#    'parking_entrance', 'pub', 'fuel', 'bicycle_parking', 'school',
-#    'pharmacy', 'recycling', 'waste_disposal', 'waste_basket',
-#    'car_wash', 'dentist', 'parking_space', 'doctors', 'parking',
-#    'public_building', 'post_office', 'atm', 'childcare', 'bar',
-#    'ice_cream', 'bus_station', 'community_centre', 'library',
-#    'bicycle_rental', 'drinking_water', 'shelter', 'clinic',
-#    'public_bookcase', 'university', 'dojo', 'kindergarten',
-#    'ferry_terminal', 'cinema', 'theatre', 'charging_station',
-#    'car_rental', 'car_sharing', 'fountain', 'seaplane terminal',
-#    'bicycle_repair_station', 'food_court', 'veterinary',
-#    'smoking_area', 'post_depot', 'locker', 'bureau_de_change',
-#    'clock', 'nightclub', 'animal_boarding', 'motorcycle_parking',
-#    'place_of_worship', 'taxi', 'shower', 'arts_centre',
-#    'social_facility', 'prep_school', 'college', 'stripclub',
-#    'construction', 'boat_rental', 'letter_box', 'social_centre',
-#    'fire_station', 'police', 'conference_centre', 'gambling',
-#    'marketplace', 'karaoke_box', 'music_school', 'compressed_air',
-#    'family_centre', 'townhall', 'sanitary_dump_station', 'studio',
-#    'training', 'trolley_bay', 'playground', 'courthouse', 'spa',
-#    'loading_dock', 'meditation_centre', 'bistro', 'language_school',
-#    'events_venue', 'parcel_locker', 'healthcare', 'binoculars',
-#    'vacuum_cleaner', 'waste_basket;recycling', 'hookah_lounge',
-#    'trash', 'internet_cafe', 'driving_school', 'disused:restaurant',
-#    'water_point', 'casino', 'science', 'ATLAS_clean_room', 'workshop',
-#    'safety', 'lobby', 'animal_shelter', 'lounger', 'storage_rental',
-#    'vacuum', 'give_box', 'atm;bank', 'EVSE', 'first_aid',
-#    'ranger_station', 'dance', 'weighbridge', 'housing co-op',
-#    'research_institute', 'monastery', 'nursing_home',
-#    'payment_terminal', 'Observation Platform', 'dive_centre',
-#    'dressing_room', 'laboratory', 'cooking_school', 'hunting_stand',
-#    'money_transfer', 'biergarten', 'mortuary',
-#    'waste_transfer_station', 'toy_library', 'animal_training',
-#    'lounge', 'hospital', 'bear_box', 'table', 'motorcycle_rental',
-#    'leisure', 'prison']
+# array(['sports_centre', 'park', 'fitness_centre', 'dance', 'playground',
+#        'swimming_pool', 'ice_rink', 'fitness_station', 'pitch', 'slipway',
+#        'escape_game', 'marina', 'hackerspace', 'bowling_alley',
+#        'Paintball', 'garden', 'golf_course', 'outdoor_seating', 'sauna',
+#        'trampoline_park', 'disc_golf_course', 'amusement_arcade',
+#        'indoor_play', 'sports_hall', 'dog_park', 'miniature_golf'],
 
 # Remove 'prison', 'money_transfer', 'storage_rental', 'language_school', 
 
