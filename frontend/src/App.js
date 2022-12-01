2e870ee9981a1491254c02458552abcd7df28214
Jooyoung Lee <jylee@WIN-VT8L7KV2994.localdomain>
2022-11-30 19:35:35 -0800

    handleSubmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,265 +1,3 @@
-<<<<<<< HEAD
-import {
-  Button, 
-  FormLabel,
-  FormControl,
-  FormGroup,
-  FormControlLabel,
-  FormHelperText,
-  Checkbox,
-  Stack,
-  Slider,
-  Box,
-  Switch,
-  Card,
-  CardHeader,
-  Divider,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Grid
-} from '@mui/material';
-
-import {useState} from 'react';
-
-function valuetext(value) {
-  return `$${value}`;
-}
-
-function not(a, b) {
-  return a.filter((value) => b.indexOf(value) === -1);
-}
-
-function intersection(a, b) {
-  return a.filter((value) => b.indexOf(value) !== -1);
-}
-
-function union(a, b) {
-  return [...a, ...not(b, a)];
-}
-
-function App() {
-  const [state, setState] = useState({
-    entire: true,
-    pv: false,
-    shared: false,
-  });
-
-  const handleChange = (event) => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.checked,
-    });
-  };
-
-  const { entire, pv, shared } = state;
-  const error = [entire, pv, shared].filter((v) => v).length !== 2;
-
-  const handleSubmit = () => {
-    const data = ['sdf']
-    console.log("submitted")
-    fetch("http://localhost:8000/airbnb_list")
-      .then((res) => console.log(res))
-      .catch((e) => console.error(e))
-  }
-
-  const [value, setValue] = useState([20, 37]);
-
-  const handleSliderChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const [stateSwitches, setStateSwitches] = useState({
-    attraction: true,
-    food: false,
-    transportation: true,
-  });
-
-  const handleSwitchesChange = (event) => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.checked,
-    });
-  };
-
-  const [checked, setChecked] = useState([]);
-  const [left, setLeft] = useState([0, 1, 2, 3]);
-  const [right, setRight] = useState([4, 5, 6, 7]);
-
-  const leftChecked = intersection(checked, left);
-  const rightChecked = intersection(checked, right);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  const numberOfChecked = (items) => intersection(checked, items).length;
-
-  const handleToggleAll = (items) => () => {
-    if (numberOfChecked(items) === items.length) {
-      setChecked(not(checked, items));
-    } else {
-      setChecked(union(checked, items));
-    }
-  };
-
-  const handleCheckedRight = () => {
-    setRight(right.concat(leftChecked));
-    setLeft(not(left, leftChecked));
-    setChecked(not(checked, leftChecked));
-  };
-
-  const handleCheckedLeft = () => {
-    setLeft(left.concat(rightChecked));
-    setRight(not(right, rightChecked));
-    setChecked(not(checked, rightChecked));
-  };
-
-  const customList = (title, items) => (
-    <Card>
-      <CardHeader
-        sx={{ px: 2, py: 1 }}
-        avatar={
-          <Checkbox
-            onClick={handleToggleAll(items)}
-            checked={numberOfChecked(items) === items.length && items.length !== 0}
-            indeterminate={
-              numberOfChecked(items) !== items.length && numberOfChecked(items) !== 0
-            }
-            disabled={items.length === 0}
-            inputProps={{
-              'aria-label': 'all items selected',
-            }}
-          />
-        }
-        title={title}
-        subheader={`${numberOfChecked(items)}/${items.length} selected`}
-      />
-      <Divider />
-      <List
-        sx={{
-          width: 200,
-          height: 230,
-          bgcolor: 'background.paper',
-          overflow: 'auto',
-        }}
-        dense
-        component="div"
-        role="list"
-      >
-        {items.map((value) => {
-          const labelId = `transfer-list-all-item-${value}-label`;
-
-          return (
-            <ListItem
-              key={value}
-              role="listitem"
-              button
-              onClick={handleToggle(value)}
-            >
-              <ListItemIcon>
-                <Checkbox
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{
-                    'aria-labelledby': labelId,
-                  }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`List item ${value + 1}`} />
-            </ListItem>
-          );
-        })}
-        <ListItem />
-      </List>
-    </Card>
-  );
-
-  return (
-    <Stack>
-    <h3>Airbnb Finder</h3>
-    
-    <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-      <FormLabel component="legend">1. Select Your Room Type</FormLabel>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Checkbox checked={entire} onChange={handleChange} name="entire" />
-          }
-          label="Entire home/apt"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox checked={pv} onChange={handleChange} name="pv" />
-          }
-          label="Private room"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox checked={shared} onChange={handleChange} name="shared" />
-          }
-          label="Shared room"
-        />
-      </FormGroup>
-      <FormLabel component="legend">2. Select Your Price Range</FormLabel>
-      <Box sx={{ width: 300 }}>
-        <Slider
-          getAriaLabel={() => 'Price range'}
-          value={value}
-          onChange={handleSliderChange}
-          valueLabelDisplay="auto"
-          getAriaValueText={valuetext}
-        />
-      </Box>
-    <FormLabel component="legend">3. Preference</FormLabel>
-    <Grid container spacing={2} justifyContent="center" alignItems="center">
-      <Grid item>{customList('Choices', left)}</Grid>
-      <Grid item>
-        <Grid container direction="column" alignItems="center">
-          <Button
-            sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
-            onClick={handleCheckedRight}
-            disabled={leftChecked.length === 0}
-            aria-label="move selected right"
-          >
-            &gt;
-          </Button>
-          <Button
-            sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
-            onClick={handleCheckedLeft}
-            disabled={rightChecked.length === 0}
-            aria-label="move selected left"
-          >
-            &lt;
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid item>{customList('Chosen', right)}</Grid>
-    </Grid>
-    </FormControl>
-    <Button variant="contained" onClick={handleSubmit}>Find!</Button>
-    </Stack>
-  );
-}
-
-export default App;
-
-=======
 import {
   Button, 
   FormLabel,
@@ -312,17 +50,16 @@ function App() {
   const error = [entire, pv, shared].filter((v) => v).length !== 2;
 
   const handleSubmit = () => {
-    const data = ['sdf']
+    const data = [state, minprice, maxprice, right]
+    console.log(data)
     console.log("submitted")
-    fetch("http://localhost:8000/airbnb_list") // TODO: 
+    fetch("http://localhost:8000/airbnb_list", ) // TODO: 
       .then((res) => console.log(res))
       .catch((e) => console.error(e))
   }
 
-  const [name, setName] = useState(0);
-  const handleTextChange = (event) => {
-    setName(event.target.value);
-  };
+  const [minprice, setMinPrice] = useState(0);
+  const [maxprice, setMaxPrice] = useState(0);
 
   const [checked, setChecked] = useState([]);
   const [left, setLeft] = useState(['Food','Attraction','Public Transporation']);
@@ -468,11 +205,12 @@ function App() {
         <TextField
           id="min price"
           label="Min Price ($CAD)"
-          onChange={handleTextChange}
+          onChange={(e) => setMinPrice(e.target.value)}
         />
         <TextField
           id="max price"
           label="Max Price ($CAD)"
+          onChange={(e) => setMaxPrice(e.target.value)}
         />
       </Box>
     </div>
@@ -514,4 +252,3 @@ function App() {
 }
 
 export default App;
->>>>>>> b732cc78962bd057c445a9173aa60780a2d5f59d
