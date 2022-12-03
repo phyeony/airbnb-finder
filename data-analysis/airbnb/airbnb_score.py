d10f6ec3755aa0981e8564eb18c0d80fc4b76904
phyeony <hyeonyoung_park@sfu.ca>
2022-12-03 12:04:54 -0800

    plot intsections

--- data-analysis/airbnb/airbnb_score.py
+++ data-analysis/airbnb/airbnb_score.py
@@ -4,8 +4,6 @@ import sys
 from airbnb_filtering import filtering
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
-import contextily as cx
-import osmnx as ox
 import shapely
 import matplotlib.pyplot as plt
 
@@ -25,12 +23,12 @@ at2 = ["transportation", "attraction"]
 
 up1 = user_preference(pr, rt2, at1)
 
-def count_overlapping_features(gdf):
+def count_overlapping_features(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     #generating all of the split pieces
     import shapely
     bounds = gdf.geometry.exterior.unary_union
     new_polys = list(shapely.ops.polygonize(bounds))
-    new_gdf = gpd.GeoDataFrame(geometry=new_polys)
+    new_gdf = gpd.GeoDataFrame(geometry=new_polys, crs='epsg:32634')
     new_gdf['id'] = range(len(new_gdf))
 
     #count overlapping by sjoin between pieces centroid and the input gdf 
@@ -58,15 +56,19 @@ gdfood = gdfood.to_crs("EPSG:32634")
 #gdf.explore(m=m)
 gdfood['geometry'] = gdfood.buffer(300, 3)
 #gdfood.explore(m=m, style_kwds={'stroke':False, 'fillOpacity':0.5})
-
+#%%
 # Count the overlapping features of restaurant buffers
 food_intersections = count_overlapping_features(gdfood)
+#%%
+avg = food_intersections['count'].mean() # 32.36
+
 food_intersections = food_intersections.sort_values('count', ascending=False)
 food_intersections['geometry'] = food_intersections.centroid
-food_intersections = food_intersections[food_intersections['count'] >= 50]
+food_intersections = food_intersections[food_intersections['count'] >= avg]
 
 # Plot results
-food_intersections.explore(m=m, marker_type = 'circle', marker_kwds = {'radius':30, 'fill':True})
+food_intersections.explore(m=m)
+#food_intersections.explore(m=m, marker_type = 'circle', marker_kwds = {'radius':30, 'fill':True})
 #gdfood.explore(m=m, style_kwds={'stroke':False, 'fillOpacity':0.5})
 # 2. filter with amenities
 # 1) for each intersection,
