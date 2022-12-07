fb564ede5375bef84162965993bac8642f39efe6
phyeony <hyeonyoung_park@sfu.ca>
2022-12-06 21:07:41 -0800

    combine stop and station

--- data-analysis/transportation/clean.py
+++ data-analysis/transportation/clean.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 
 stop = pd.read_csv("Stops.csv")
@@ -6,5 +7,16 @@ station = pd.read_csv("Stations.csv")
 del stop['x'], stop['y']
 del station['x'], station['y']
 
-stop.to_csv('cleaned_stops.csv')
-station.to_csv('cleaned_stations.csv')
+
+stop = stop[['stop_name','Longitude','Latitude']]
+station = station[['Exch_Name','Longitude','Latitude']]
+
+stop.rename(columns={'stop_name': 'name'}, inplace=True)
+station.rename(columns={'Exch_Name': 'name'}, inplace=True)
+combined = pd.concat([stop, station], axis=0)
+
+#combined.to_cvs("cleaned_public_transporation.csv")
+print(combined)
+combined.to_csv("cleaned_public_transportation.csv")
+# station.to_csv('cleaned_stations.csv')
+# %%
